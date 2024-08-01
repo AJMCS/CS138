98910be3c659e3434f55e47192945144a7b64637
AJ McDaniel <49733674+AJMCS@users.noreply.github.com>
2024-07-31 21:36:31 -0700

    Add files via upload

---
+++ Labs/Lab 12/Lab-12-VaccineRecord-AJ-McDaniel/main.py
@@ -0,0 +1,7 @@
+import vaccine_record
+
+def main():
+  pass #replace with your tests
+
+if __name__ == '__main__':
+  main()
---
+++ Labs/Lab 12/Lab-12-VaccineRecord-AJ-McDaniel/vaccine_record.py
@@ -0,0 +1,19 @@
+# The VaccineRecord encapsulates the COVID-19 vaccine data (doses 1 and 2) for a person.
+'''
+VaccineRecord
+------------------------------
+- first_name : string
+- last_name : string
+- dob : string
+- d1_manufacturer : string
+- d1_batch : string
+- d1_date : string
+- d1_location : string
+- d2_manufacturer : string
+- d2_batch : string
+- d2_date : string
+- d2_location : string
+------------------------------
+ 
+'''
+
