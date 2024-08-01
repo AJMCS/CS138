bcac7936cc2587b338684eb7b9627a76d381bc7f
AJ McDaniel <49733674+AJMCS@users.noreply.github.com>
2024-07-31 21:37:45 -0700

    Add files via upload

---
+++ Labs/Lab 14/Lab-14-VaccineRecord-with-GUI-AJ-McDaniel/main.py
@@ -0,0 +1,12 @@
+from tkinter import * # gui module
+import vaccine_record 
+
+#this method should be called when "Submit"   button is clicked
+def process():
+  print("submit was clicked")
+
+#this method should be called when checkbox is ticked/unticked. Have an IF for off, ELSE   for on
+def checkbox_process():
+  print("checkbox interaction")
+
+#begin code for GUI 
