cac5fa5da32cd5493e3d284df6c7a03758f7ae32
AJ McDaniel <49733674+AJMCS@users.noreply.github.com>
2024-07-31 21:13:23 -0700

    Add files via upload

---
+++ Labs/Lab 01/Lab-01-Hello-MiraCostan-user1920250/main.py
@@ -0,0 +1,18 @@
+'''
+DEVELOPER: A.J. McDaniel
+COLLABORATOR(S): N/A
+'''
+"""
+This program provides information to a MiraCosta College student.
+"""
+nameOfService = "Name of service: STEM LEARNING CENTERS"
+contactInfo = "Contact info: OC1200 Administration Building, 760.795.6682, https://www.miracosta.edu/student-services/stemlc/index.html"
+description = "Description: Our Mission Statement: The MiraCosta STEM Learning Centers provide free and superior student support to all MiraCosta students in STEM (Science, Technology, Engineering, and Mathematics) courses utilizing a friendly, inviting environment with qualified instructors, professional subject matter experts, and trained tutors and staff. "
+
+
+
+print("Hello MiraCostan!")
+print("Did you know MiraCosta has services to help students succeed? Here is information about one of them.")
+print(nameOfService)
+print(contactInfo)
+print(description)
