d73520b1259c218985ab630d595b4c0ab7c18c99
AJ McDaniel <49733674+AJMCS@users.noreply.github.com>
2024-07-31 21:31:58 -0700

    Add files via upload

---
+++ Labs/Lab 10/Lab-10-Exam-Average-and-Graph-AJ-McDaniel/main.py
@@ -0,0 +1,13 @@
+'''
+DEVELOPER: your name
+COLLABORATOR(S): name(s) of anyone who helped you
+'''
+""" This program reads grades from a file, calculates averages, then creates a bar graph.
+"""
+##### IMPORTS #####
+
+##### CONSTANT VARIABLES #####
+
+##### FUNCTION DEFINITIONS #####
+
+##### MAIN PROGRAM #####
