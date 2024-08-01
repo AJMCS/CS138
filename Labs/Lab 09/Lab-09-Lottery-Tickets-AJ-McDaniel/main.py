d7dce10eb5200d7e8f4cd5ae8e4c01b46e42cc0c
AJ McDaniel <49733674+AJMCS@users.noreply.github.com>
2024-07-31 21:30:59 -0700

    Add files via upload

---
+++ Labs/Lab 09/Lab-09-Lottery-Tickets-AJ-McDaniel/main.py
@@ -0,0 +1,14 @@
+'''
+DEVELOPER: your name
+COLLABORATOR(S): name(s) of anyone who helped you
+'''
+""" This program is to create lottery cards. The user can select their own numbers or have the numbers randomly selected for them.
+"""
+##### IMPORTS #####
+
+
+##### FUNCTION DEFINITIONS #####
+
+  
+##### MAIN PROGRAM #####
+
